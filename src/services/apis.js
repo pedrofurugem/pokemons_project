a89d3a5147401af7482aab23f1b9e354ab9a00bb
Pedro Roberto <pedrofuruguem@hotmail.com>
2022-12-30 16:37:12 -0300

    atualizando

--- src/services/apis.js
+++ src/services/apis.js
@@ -2,7 +2,7 @@ async function getPokemonList(limit, offSet){
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offSet}`)
     const json = await response.json()
     const names = await json.results.map(pokemon => { return pokemon.name}) 
-    return names
+    return await names
 }
 
 async function getPokemon(name){
